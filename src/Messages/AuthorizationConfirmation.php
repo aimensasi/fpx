bca47e0c96aa26dcb4c910c973c80afd356d7076
AIMEN .S.A.SASI <aimen.s.a.sasi@AIMENs-MacBook-Air-2.local>
2021-05-25 17:10:07 +0800

    Work in progress

--- src/Messages/AuthorizationConfirmation.php
+++ src/Messages/AuthorizationConfirmation.php
@@ -28,28 +28,28 @@ class AuthorizationConfirmation extends Message implements Contract {
 	 * @return mixed
 	 */
 	public function handle($options) {
-		$this->targetBankBranch = $options['fpx_buyerBankBranch'];
-		$this->targetBankId = $options['fpx_buyerBankId'];
-		$this->buyerIBAN = $options['fpx_buyerIban'];
-		$this->buyerId = $options['fpx_buyerId'];
-		$this->buyerName = $options['fpx_buyerName'];
-		$this->creditResponseStatus = $options['fpx_creditAuthCode'];
-		$this->creditResponseNumber = $options['fpx_creditAuthNo'];
-		$this->debitResponseStatus = $options['fpx_debitAuthCode'];
-		$this->debitResponseNumber = $options['fpx_debitAuthNo'];
-		$this->foreignId = $options['fpx_fpxTxnId'];
-		$this->foreignTimestamp = $options['fpx_fpxTxnTime'];
-		$this->makerName = $options['fpx_makerName'];
-		$this->flow = $options['fpx_msgToken'];
-		$this->type = $options['fpx_msgType'];
-		$this->exchangeId = $options['fpx_sellerExId'];
-		$this->id = $options['fpx_sellerExOrderNo'];
-		$this->sellerId = $options['fpx_sellerId'];
-		$this->reference = $options['fpx_sellerOrderNo'];
-		$this->timestamp = $options['fpx_sellerTxnTime'];
-		$this->amount = $options['fpx_txnAmount'];
-		$this->currency = $options['fpx_txnCurrency'];
-		$this->checkSum = $options['fpx_checkSum'];
+		$this->targetBankBranch = @$options['fpx_buyerBankBranch'];
+		$this->targetBankId = @$options['fpx_buyerBankId'];
+		$this->buyerIBAN = @$options['fpx_buyerIban'];
+		$this->buyerId = @$options['fpx_buyerId'];
+		$this->buyerName = @$options['fpx_buyerName'];
+		$this->creditResponseStatus = @$options['fpx_creditAuthCode'];
+		$this->creditResponseNumber = @$options['fpx_creditAuthNo'];
+		$this->debitResponseStatus = @$options['fpx_debitAuthCode'];
+		$this->debitResponseNumber = @$options['fpx_debitAuthNo'];
+		$this->foreignId = @$options['fpx_fpxTxnId'];
+		$this->foreignTimestamp = @$options['fpx_fpxTxnTime'];
+		$this->makerName = @$options['fpx_makerName'];
+		$this->flow = @$options['fpx_msgToken'];
+		$this->type = @$options['fpx_msgType'];
+		$this->exchangeId = @$options['fpx_sellerExId'];
+		$this->id = @$options['fpx_sellerExOrderNo'];
+		$this->sellerId = @$options['fpx_sellerId'];
+		$this->reference = @$options['fpx_sellerOrderNo'];
+		$this->timestamp = @$options['fpx_sellerTxnTime'];
+		$this->amount = @$options['fpx_txnAmount'];
+		$this->currency = @$options['fpx_txnCurrency'];
+		$this->checkSum = @$options['fpx_checkSum'];
 
 		try {
 			$this->verifySign($this->checkSum, $this->format());
